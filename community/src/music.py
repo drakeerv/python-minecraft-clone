7cca844a7ede44bb2d0643b4f7481a4b4b4e5ebc
drakeerv <drakeerv@outlook.com>
2024-08-11 01:04:07 -0400

    Fix formatting

--- community/src/music.py
+++ community/src/music.py
@@ -1,8 +1,9 @@
 import pyglet.media
 
+
 class MusicPlayer(pyglet.media.Player):
-    def __init__(self):
-        super().__init__()
-        
-        self.standby = False
-        self.next_time = 0
+	def __init__(self):
+		super().__init__()
+
+		self.standby = False
+		self.next_time = 0
